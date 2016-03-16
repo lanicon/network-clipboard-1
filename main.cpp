a11f4b8877b9408c2e8bdd414808515f4dd3fd45
Thomas <t.schraven@student.fontys.nl>
2016-03-16 11:49:23 +0100

    fix for mingw bug 2250

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 #include "networkclipboard.h"
 #include "qmlclipboardadapter.h"
 
+// fix for mingw bug 2250
+#if defined(_WIN32) || defined(_WIN64)
+	#define __NO_INLINE__
+#endif
+
 int main(int argc, char *argv[])
 {
     QTime time = QTime::currentTime();
